62837f5e2009605c333683b5decd1ac6ec5cb727
Toky-Lionnel <tokyrajaonarivony@gmail.com>
2026-04-08 11:35:43 +0300

    données de test pour line chart

---
+++ sql/08-04-2026.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `budget` (`customer_id`, `amount`, `created_at`) VALUES
+(1, 10000.00, '2026-04-25 10:00:00'),
+(2, 15000.00, '2026-04-25 10:00:00'),
+(3, 20000.00, '2026-04-25 10:00:00');
+
+SELECT customer_id FROM `customer`;
